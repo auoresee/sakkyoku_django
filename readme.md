7543fc2c1d4a904bf0517115e9f256e380879dcb
BuildTools <mariodragonquest1010@yahoo.co.jp>
2021-12-02 01:01:07 +0900

    Security update

--- readme.md
+++ readme.md
@@ -2,9 +2,10 @@
 2. スーパーユーザー noverdi を作成
         python3 manage.py createsuperuser
 3. mysqlpassword_.pyにパスワードを入力し、mysqlpassword.pyにファイル名を変更する
-4. python3 manage.py makemigrations
-5. python3 manage.py migrate
-6. サーバーを起動し、データベースを構築する
+4. secretkey_.pyに秘密鍵を入力し、secretkey.pyに変更
+5. python3 manage.py makemigrations
+6. python3 manage.py migrate
+7. サーバーを起動し、データベースを構築する
    /admin/db/にアクセスし、SQLパスワードを入力
    
 # サーバーの起動
--- sakkyoku/settings.py
+++ sakkyoku/settings.py
@@ -13,16 +13,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 from . import mysqlpassword
+from . import secretkey
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1d0l4sb2d!hjuo0i^=(hqwbe-c_#i$7s(^he&%!s7%1)l&zmp1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
